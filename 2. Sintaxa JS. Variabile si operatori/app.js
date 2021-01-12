917d31e9bfe420276e6cdf62119b096227c50b3e
Sergiu Chilat <chilatsergiu@gmail.com>
2021-01-12 19:34:52 +0200

    Sintaxa JS. Variabile si operatori

---
+++ 2. Sintaxa JS. Variabile si operatori/app.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+ 
+    let a = 2;
+    let b = 3;
+
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(a / b);
+
+    const pi = 3.14;
+    let r = 2;
+    console.log(pi * r**2);
+
+
+    let word1 = "Hello";
+    let word2 = "world!"
+
+    console.log(word1 + word2);
+
+    let number = 2;
+    let string = "string";
+
+    console.log(number + string);
+
+});
