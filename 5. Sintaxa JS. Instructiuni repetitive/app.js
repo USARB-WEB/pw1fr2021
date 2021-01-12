87b15319ca96a931fb8af541ceea1ccf1bf80825
Sergiu Chilat <chilatsergiu@gmail.com>
2021-01-12 19:55:38 +0200

    Sintaxa JS. Instructiuni repetitive

---
+++ 5. Sintaxa JS. Instructiuni repetitive/app.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+   
+    for (let index = 0; index < 100; index++) {
+        console.log(index);
+    }
+
+    let index = 0;
+    while ( index < 100) {
+        console.log(index);
+        index++;
+    }
+
+    do {
+        console.log(index);
+        index++;
+    } while ( index < 100)
+
+    
+    
+});
